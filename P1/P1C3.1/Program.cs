d895f80af63a14fa983bbce1e30c9fbc9a2b3c5b
EmeDes19 <emericdesmarais02@yahoo.ca>
2024-11-10 20:43:14 -0500

    Update Program.cs

--- P1/P1C3.1/Program.cs
+++ P1/P1C3.1/Program.cs
@@ -4,5 +4,5 @@ int denominateur = 4;
 // TODO : modifier l'instruction ci-dessous pour que la réponse prenne une valeur décimale
 double reponse = numerateur / (double) denominateur;
 
-// Afficher le résultat 
+// Afficher le résultatt
 Console.WriteLine(numerateur + " / " + denominateur + " = " + reponse);
